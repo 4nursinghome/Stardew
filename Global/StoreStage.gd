e9fe605929f76bd2047411a7fc021a3072ac9c8d
Chouchzh <138787818+Chouchzh@users.noreply.github.com>
2025-05-24 17:25:18 +0800

    Add files via upload
    更新進度

---
+++ Global/StoreStage.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var harvest_data := {"carrot":999,
+					"cabbage":999,
+					"beet":999,
+					"tomato":999,
+					"corn":999,
+					"pumpkin":999,
+					"onion":999,
+					"broccoli":999,
+					"cat":999}  # 儲存每種作物的收成量，例如 {"tomato": 3, "cabbage": 1}
+
+func add_crop(crop_name: String, amount: int = 1):
+	if harvest_data.has(crop_name):
+		harvest_data[crop_name] += amount
+	else:
+		harvest_data[crop_name] = amount
+	print("✅ 收成 ", crop_name, " x", amount, "（目前總數：", harvest_data[crop_name], "）")
+
+func get_crop_count(crop_name: String) -> int:
+	return harvest_data.get(crop_name, 0)

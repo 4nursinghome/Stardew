9d704e7250325565a774ee7bf5eec011dfe57ad0
Chouchzh <138787818+Chouchzh@users.noreply.github.com>
2025-05-25 16:30:02 +0800

    Add files via upload
    re

--- Global/StoreStage.gd
+++ Global/StoreStage.gd
@@ -15,7 +15,7 @@ func add_crop(crop_name: String, amount: int = 1):
 		harvest_data[crop_name] += amount
 	else:
 		harvest_data[crop_name] = amount
-	print("✅ 收成 ", crop_name, " x", amount, "（目前總數：", harvest_data[crop_name], "）")
+	print("✅ harvest ", crop_name, " x", amount, "（Total：", harvest_data[crop_name], "）")
 
 func get_crop_count(crop_name: String) -> int:
 	return harvest_data.get(crop_name, 0)

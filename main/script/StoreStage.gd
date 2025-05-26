4523004a24a1f13352bd0f2e0b79c6dc67bca8dd
NathanOuO <120110038+NathanOuO@users.noreply.github.com>
2025-05-26 16:50:34 +0800

    Add files via upload

--- main/script/StoreStage.gd
+++ main/script/StoreStage.gd
@@ -1,13 +1,25 @@
 extends Node
 
-var harvest_data := {}  # 儲存每種作物的收成量，例如 {"tomato": 3, "cabbage": 1}
+signal harvest_updated
+var harvest_data := {"carrot":0,
+					"cabbage":0,
+					"beet":0,
+					"tomato":0,
+					"corn":0,
+					"pumpkin":0,
+					"onion":0,
+					"broccoli":0,
+					"cat":0}  # 儲存每種作物的收成量，例如 {"tomato": 3, "cabbage": 1}
 
 func add_crop(crop_name: String, amount: int = 1):
 	if harvest_data.has(crop_name):
 		harvest_data[crop_name] += amount
+		emit_signal("harvest_updated")
 	else:
 		harvest_data[crop_name] = amount
+		emit_signal("harvest_updated")
 	print("✅ 收成 ", crop_name, " x", amount, "（目前總數：", harvest_data[crop_name], "）")
+
 
 func get_crop_count(crop_name: String) -> int:
 	return harvest_data.get(crop_name, 0)

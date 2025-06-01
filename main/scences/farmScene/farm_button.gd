355dac18d09575dd98012b8532dc3074a7f58eb6
NathanOuO <120110038+NathanOuO@users.noreply.github.com>
2025-06-02 05:40:29 +0800

    Add files via upload

--- main/scences/farmScene/farm_button.gd
+++ main/scences/farmScene/farm_button.gd
@@ -2,9 +2,10 @@ extends Control
 signal home_pressed(button_name)
 
 @onready var home_button = $homeButton
-
+@onready var storage_button=$storageButton
+@onready var borad_button=$boardButton
 func _ready():
-	for button in [home_button]:
+	for button in [home_button,storage_button,borad_button]:
 		button.pressed.connect(_on_button_pressed)
 
 func _on_button_pressed(button_name):
